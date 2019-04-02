9375b21f4bac5d1a01eb853b2073f8dfeea6edfa
zohaibnoor <zohaibnr@gmail.com>
2019-04-02 22:21:50 +0500

    First git commit for my Django code

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nordicomm/Documents/My Learning so Far/DemoVsProject/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
